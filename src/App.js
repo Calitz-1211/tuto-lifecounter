86444b93012c4a2d4665523f3561b9e5816aa300
Nono <arnaud_deguerry@gmail.com>
2023-02-20 16:45:55 +0100

    ajout du bouton pour cacher le hero

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import { Chip } from './component/chips';
-import { Header } from './component/header';
+import "./App.css";
+import { Header } from "./component/header";
+import HideDiv from "./component/HideDiv"; 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and oui.
-        </p>
-        <Chip value={1}/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </div>
+      <Header />
+      <HideDiv/>
     </div>
   );
 }
---
+++ src/component/header/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+export const Header = (props) => {
+    
+    return (
+        <div className='header'>
+            <div className='flex'>
+                <p div className='texte'>Cacpot Non-Solver...</p>
+            </div>
+            <div className='flex'>
+                <p className='texte2'> at least I tried</p>
+            </div>
+        </div>
+    )
+}
